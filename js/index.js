213f167035b75b9607cdebc6c0e4267a0ca67924
Baly <baly818p@gmail.com>
2016-07-09 11:20:53 +0300

    all commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  	auto: true,
+  	
+  });
+});
+$(document).ready(function(){
+  $('.slider2').bxSlider({
+    slideWidth: 220,
+    minSlides: 4,
+    maxSlides: 4,
+    moveSlides: 1,
+    slideMargin: 40
+  });
+});
+
